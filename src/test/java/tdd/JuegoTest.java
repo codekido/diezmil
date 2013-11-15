c1a49f1985e798e59232266a63bd592f0585e63f
ramiror <ramiro.rela+git@gmail.com>
2013-11-14 23:04:36 -0300

    testElUnoVale100

--- src/test/java/tdd/JuegoTest.java
+++ src/test/java/tdd/JuegoTest.java
@@ -23,5 +23,11 @@ public class JuegoTest {
 			assertEquals(String.format("El jugador '%s' debería tener 0 puntos", j.nombre()), 0, j.puntos());
 		}
 	}
+	
+	@Test
+	public void testElUnoVale100() {
+		Juego juego = new Juego();
+		assertEquals("El primer jugador debería tener 100 puntos por el uno", 100, juego.forzar(1, 2, 3, 4, 4).jugador(1).puntos());
+	}
 
 }
