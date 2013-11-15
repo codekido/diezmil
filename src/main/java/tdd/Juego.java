bc723623af0a2709e9331e14b7b9834409690d18
dariomatias <dario@gmail.com>
2013-11-14 21:48:52 -0600

    Resuelto el test inicial. Creada clase Juego y Jugador con los métodos Puntos y Nombre.

---
+++ src/main/java/tdd/Juego.java
@@ -0,0 +1,12 @@
+package tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Juego {
+
+	public List<Jugador> jugadores() {
+		return new ArrayList<Jugador>();
+	}
+
+}
---
+++ src/main/java/tdd/Jugador.java
@@ -0,0 +1,13 @@
+package tdd;
+
+public class Jugador {
+
+	public String nombre() {
+		return "Jugador";
+	}
+
+	public int puntos() {
+		return 0;
+	}
+
+}
