f6454fa69af892c6c4b940fe6a0caea859e74419
ramiror <ramiro.rela+git@gmail.com>
2013-11-17 11:18:48 -0300

    Agrego 5 cincos valen 500 Hubo algo raro con el UTF-8. Comiteando una property para dejarlo en UTF-8. Quizás haya que rescribir todos los acentos.

--- src/test/java/tdd/JuegoTest.java
+++ src/test/java/tdd/JuegoTest.java
@@ -22,54 +22,57 @@ public class JuegoTest {
 	@Test
 	public void testTodosEmpiezanConCeroPuntos() {
 		for (Jugador j : juego.jugadores()) {
-			assertEquals(String.format("El jugador '%s' debería tener 0 puntos.", j.nombre()), 0, j.puntos());
+			assertEquals(String.format("El jugador '%s' deber��a tener 0 puntos.", j.nombre()), 0, j.puntos());
 		}
 	}
 	
 	@Test
 	public void testElUnoVale100() {
-		assertEquals("El primer jugador debería tener 100 puntos por el uno.", 100, juego.forzar(1, 2, 3, 4, 4).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 100 puntos por el uno.", 100, juego.forzar(1, 2, 3, 4, 4).jugador(0).puntos());
 	}
 
 	@Test
 	public void testDosUnosValen200() {
-		assertEquals("El primer jugador debería tener 200 puntos por los dos unos.", 200, juego.forzar(1, 2, 3, 1, 4).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 200 puntos por los dos unos.", 200, juego.forzar(1, 2, 3, 1, 4).jugador(0).puntos());
 	}
 	
 	@Test
 	public void testTresUnosValen1000() {
-		assertEquals("El primer jugador debería tener 1000 puntos por los tres unos.", 1000, juego.forzar(1, 2, 1, 1, 4).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 1000 puntos por los tres unos.", 1000, juego.forzar(1, 2, 1, 1, 4).jugador(0).puntos());
 	}
 	
 	@Test
 	public void testCuatroUnosValen1100() {
-		assertEquals("El primer jugador debería tener 1100 puntos por los cuatro unos.", 1100, juego.forzar(1, 2, 1, 1, 1).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 1100 puntos por los cuatro unos.", 1100, juego.forzar(1, 2, 1, 1, 1).jugador(0).puntos());
 	}
 	
 	@Test
 	public void testCincoUnosValen1200() {
-		assertEquals("El primer jugador debería tener 1200 puntos por los cinco unos.", 1200, juego.forzar(1, 1, 1, 1, 1).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 1200 puntos por los cinco unos.", 1200, juego.forzar(1, 1, 1, 1, 1).jugador(0).puntos());
 	}
 	
 	@Test
 	public void testElDosVale0() {
-		assertEquals("El primer jugador debería tener 0 puntos porque no hay números especiales.", 0, juego.forzar(3, 2, 3, 4, 4).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 0 puntos porque no hay n��meros especiales.", 0, juego.forzar(3, 2, 3, 4, 4).jugador(0).puntos());
 	}
 	
 	@Test
 	public void testTresDosValen200() {
-		assertEquals("El primer jugador debería tener 200 puntos por los tres dos.", 200, juego.forzar(2, 3, 2, 3, 2).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 200 puntos por los tres dos.", 200, juego.forzar(2, 3, 2, 3, 2).jugador(0).puntos());
 	}
 
 	@Test
 	public void testTresTresValen300() {
-		assertEquals("El primer jugador debería tener 300 puntos por los tres tres.", 300, juego.forzar(2, 3, 3, 3, 2).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 300 puntos por los tres tres.", 300, juego.forzar(2, 3, 3, 3, 2).jugador(0).puntos());
 	}
 	
 	@Test
 	public void testTresCuatrosValen400() {
-		assertEquals("El primer jugador debería tener 400 puntos por los tres cuatros.", 400, juego.forzar(2, 4, 4, 4, 2).jugador(0).puntos());
+		assertEquals("El primer jugador deber��a tener 400 puntos por los tres cuatros.", 400, juego.forzar(2, 4, 4, 4, 2).jugador(0).puntos());
 	}
 	
-	
+	@Test
+	public void testTresCincosValen500() {
+		assertEquals("El primer jugador deber��a tener 400 puntos por los tres cuatros.", 500, juego.forzar(2, 5, 5, 5, 2).jugador(0).puntos());
+	}
 }
